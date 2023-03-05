9f94b0d25960777a9d2dbeab2903e9a65848b7b6
Shaik Ahmad Nawaz <nawazcode@gmail.com>
2023-03-05 13:37:40 +0530

    basic setup done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import React from "react";
+import Navbar from "./components/Navbar/Navbar";
+import Search from "./components/SearchDiv/Search";
+import Jobs from "./components/JobDiv/Jobs";
+import Value from "./components/ValueDiv/Value";
+import Footer from "./components/Footer/Footer";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div>
+      <Navbar />
+      <Search />
+      <Jobs />
+      <Value />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
