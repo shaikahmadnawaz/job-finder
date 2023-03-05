26ffdbe042d956a55aae252e71fccbadf466b1e2
Shaik Ahmad Nawaz <nawazcode@gmail.com>
2023-03-05 13:27:21 +0530

    customize

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blueColor: "#2a68ff",
+        greyIsh: "f1f4f8",
+        cardShadow: "#f7f8f9",
+        textColor: "#252b36",
+      },
+    },
   },
   plugins: [],
 };
