1b01bc7b4dc2b7bdeb2db4bdbb36bdfdd3aaa281
Shaik Ahmad Nawaz <nawazcode@gmail.com>
2023-03-05 14:25:58 +0530

    work in progress

--- src/components/SearchDiv/Search.js
+++ src/components/SearchDiv/Search.js
@@ -1,7 +1,18 @@
 import React from "react";
+import { AiOutlineSearch } from "react-icons/ai";
 
 const Search = () => {
-  return <div>Search</div>;
+  return (
+    <div className="searchDiv grid gp-10 bg-slate-200 rounded-[10px] p-[3rem]">
+      <form action="">
+        <div className="firstDiv flex justify-between items-center rounded-[8px] gap-[10px] bg-white p-5 shadow-lg shadow-greyIsh-700">
+          <div className="flex gap-2 items-center">
+            <AiOutlineSearch className="text-[25px] icon" />
+          </div>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default Search;
