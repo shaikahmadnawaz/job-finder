6ac67b7699fdb94570993e2a55746ee19b47c4a3
Shaik Ahmad Nawaz <nawazcode@gmail.com>
2023-03-05 16:27:18 +0530

    search form done

--- src/components/SearchDiv/Search.js
+++ src/components/SearchDiv/Search.js
@@ -1,5 +1,7 @@
 import React from "react";
-import { AiOutlineSearch } from "react-icons/ai";
+import { AiOutlineCloseCircle, AiOutlineSearch } from "react-icons/ai";
+import { HiOutlineBuildingOffice2 } from "react-icons/hi2";
+import { CiLocationOn } from "react-icons/ci";
 
 const Search = () => {
   return (
@@ -8,7 +10,37 @@ const Search = () => {
         <div className="firstDiv flex justify-between items-center rounded-[8px] gap-[10px] bg-white p-5 shadow-lg shadow-greyIsh-700">
           <div className="flex gap-2 items-center">
             <AiOutlineSearch className="text-[25px] icon" />
+            <input
+              type="text"
+              className="bg-transparent text-blue-500 focus:outline-none w-[100%]"
+              placeholder="Search Job Here...."
+            />
+            <AiOutlineCloseCircle className="text-[30px] text-[#a5a6a6] hover:text-textColor icon" />
           </div>
+
+          <div className="flex gap-2 items-center">
+            <HiOutlineBuildingOffice2 className="text-[25px] icon" />
+            <input
+              type="text"
+              className="bg-transparent text-blue-500 focus:outline-none w-[100%]"
+              placeholder="Search By Company...."
+            />
+            <AiOutlineCloseCircle className="text-[30px] text-[#a5a6a6] hover:text-textColor icon" />
+          </div>
+
+          <div className="flex gap-2 items-center">
+            <CiLocationOn className="text-[25px] icon" />
+            <input
+              type="text"
+              className="bg-transparent text-blue-500 focus:outline-none w-[100%]"
+              placeholder="Search By Location...."
+            />
+            <AiOutlineCloseCircle className="text-[30px] text-[#a5a6a6] hover:text-textColor icon" />
+          </div>
+
+          <button className="bg-blueColor h-full p-5 px-10 rounded-[10px] text-white cursor-pointer hover:bg-blue-400">
+            Search
+          </button>
         </div>
       </form>
     </div>
